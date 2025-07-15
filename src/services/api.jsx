8f871f3417113fbc388da6029200b16948b1aae9
kelver <kelverwt@gmail.com>
2025-07-15 20:07:57 -0300

    modified urlconection

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const api = axios.create({ baseURL: 'http://127.0.0.1:8000/api/' },
+const api = axios.create({ baseURL: 'https://rninox-b774b03b3a02.herokuapp.com/api/' },
     {
         headers: {
             Authorization: `Bearer ${localStorage.getItem("access")}`,
